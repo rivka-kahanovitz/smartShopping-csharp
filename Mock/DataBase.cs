2391fd92514954aac217d82d46c00ec5bd678fb1
michal-genut <michal40226@gmail.com>
2025-05-01 14:59:16 +0300

    uploading project

---
+++ Mock/DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+
+
+
+namespace Mock
+{
+    public class DataBase : DbContext   
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Stores> Stores { get; set; }
+        public DbSet<Price> Prices { get; set; }
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
+        public DbSet<ShoppingListItem> ShoppingListItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-SSNMLFD;database=SmartShoppingApplication;trusted_connection=true;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ Repository/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Entities
+{
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "שם המשתמש הוא שדה חובה")]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        //דרוש הסבר :)
+        public ICollection<ShoppingList> ShoppingLists { get; set; }
+    }
+}
