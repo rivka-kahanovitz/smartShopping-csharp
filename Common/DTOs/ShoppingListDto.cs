c96001bdc103ff78101f79d1cc97d9b597eeb808
first-git <rivki400@gmail.com>
2025-06-08 18:11:48 +0300

    עדכון gitignore וניקוי קבצים לא רצויים

--- Common/DTOs/ShoppingListDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace common.DTOs
-{
-    public class ShoppingListDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } // אם יש שם לרשימה
-        public List<ShoppingListItemDto> Items { get; set; }
-    }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# SmartShopping-C#
